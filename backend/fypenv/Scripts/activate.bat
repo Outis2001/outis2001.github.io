38da7ab03bc61f491e4ff30599472056b616cd3f
Benul <benujith@gmail.com>
2025-05-22 18:28:41 +0530

    add new backend

--- backend/fypenv/Scripts/activate.bat
+++ backend/fypenv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\Benul\OneDrive - University of Moratuwa\Desktop\fyp\Dominious\backend\fypenv
+set VIRTUAL_ENV=C:\Users\laksh\Desktop\Dominious\backend\fypenv
 
 if not defined PROMPT set PROMPT=$P$G
 
--- backend/src/app/main.py
+++ backend/src/app/main.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
-from src.utills import generate_domains, postprocessing, domain_details,RAG,gemma,gemma_post_processing,gemma_decsription,gemma_preprocess
+from src.utills import generate_domains, postprocessing, domain_details
 
 app = FastAPI()
 
@@ -24,25 +24,15 @@ class DetailRequest(BaseModel):
 
 @app.post("/generate-domains/")
 async def generate_domains_endpoint(prompt: Prompt):
-
-    samples=RAG(prompt.prompt)
-    # output=gemma(prompt.prompt,samples)
-    # domain_names=gemma_post_processing(output)   # for  Gemma
-
-    domains = generate_domains(prompt.prompt,samples)
+    domains = generate_domains(prompt.prompt)
+    #print(domains)
     domain_names = postprocessing(domains)
     #print(domain_names)
-    #domain_names=RAG(prompt.prompt)
-    print(domain_names)
     return {"domains": domain_names}
 
 @app.post("/details/")
 async def get_domain_details(request: DetailRequest):
     dd=domain_details(request.domain_name,request.prompt)
-
-
-    # dd,domain_name=gemma_decsription(request.domain_name,request.prompt)
-    # dd=gemma_preprocess(dd,domain_name) # for gemma
     return dd
 
 if __name__ == "__main__":
